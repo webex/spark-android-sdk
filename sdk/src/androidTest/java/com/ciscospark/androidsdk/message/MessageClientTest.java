06fca985496a7610ed035351fddf6ddae3e5dd87
Allen Shaw <xionxiao@cisco.com>
2018-07-27 13:41:07 +0800

    [SPARK-17126]add message test cases

--- sdk/src/androidTest/java/com/ciscospark/androidsdk/message/MessageClientTest.java
+++ sdk/src/androidTest/java/com/ciscospark/androidsdk/message/MessageClientTest.java
@@ -1,73 +1,120 @@
-/*
- * Copyright 2016-2017 Cisco Systems Inc
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package com.ciscospark.androidsdk.message;
 
-import com.ciscospark.androidsdk.Spark;
-import com.ciscospark.androidsdk.auth.JWTAuthenticator;
-import com.ciscospark.androidsdk.message.internal.MessageClientImpl;
-import com.google.gson.Gson;
+import android.app.Application;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.MediumTest;
+import android.support.test.runner.AndroidJUnit4;
 
+import com.ciscospark.androidsdk.Spark;
+import com.ciscospark.androidsdk.auth.OAuthTestUserAuthenticator;
+import com.ciscospark.androidsdk.room.RoomClient;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-/**
- * Created by zhiyuliu on 02/09/2017.
- */
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+
+@RunWith(AndroidJUnit4.class)
+@MediumTest
 public class MessageClientTest {
 
-    private static String auth_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJreWxlX2RlbW8iLCJuYW1lIjoia3lsZV9kZW1vIiwiaXNzIjoiY2Q1YzlhZjctOGVkMy00ZTE1LTk3MDUtMDI1ZWYzMGIxYjZhIn0.KuabN0TWa00F2Auv4vnu8DXrXAiVM9p1dL8fJUEScbg";
-    private static String testPersonId = "Y2lzY29zcGFyazovL3VzL1BFT1BMRS83MmEyYTY4ZS1kNjc5LTRkMTUtOTdlOC1mNzRiZWViYTA2OWU";
-    private static JWTAuthenticator authenticator;
-    private static MessageClient mClient;
-    private static Spark mSpark;
-    private static boolean authCompleted = false;
+    private static String auth_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhbmRyb2lkX3Rlc3R1c2VyXzEiLCJuYW1lIjoiQW5kcm9pZFRlc3RVc2VyMSIsImlzcyI6ImNkNWM5YWY3LThlZDMtNGUxNS05NzA1LTAyNWVmMzBiMWI2YSJ9.eJ99AY9iNDhG4HjDJsY36wgqOnNQSes_PIu0DKBHBzs";
+    private static String client_id = "C338457876c27e853fb5e4dc85c6087164073e1e2bf415c081b600fe654d7af60";
+    private static String client_sec = "0dc8b9c77c8b5597e7eef4b72c7dfaf8d5b834fdba24cf0a0b55eb85ae209b31";
+    private static String redirect = "AndroidKitchenSink://response";
+    private static OAuthTestUserAuthenticator authenticator;
+    private static Context context;
+    private static Spark spark;
+    private static MessageClient messageClient;
 
     @BeforeClass
-    public static void setUp() throws Exception {
-        System.out.println("setup test case");
-        authenticator = new JWTAuthenticator();
-        authenticator.authorize(auth_token);
-        mClient = new MessageClientImpl(authenticator);
+    public static void setUpBeforeClass() throws Exception {
+        System.out.println("final set up");
+        final CountDownLatch signal = new CountDownLatch(1);
+        getInstrumentation().runOnMainSync(() -> {
+            context = InstrumentationRegistry.getContext();
+            authenticator = new OAuthTestUserAuthenticator(
+                    client_id, client_sec, "spark:all", redirect, "sparksdktestuser16@tropo.com", null, "Test(123)");
+            spark = new Spark((Application) context.getApplicationContext(), authenticator);
+            authenticator.authorize(result -> {
+                System.out.println("get result" + result.toString());
+                if (authenticator.isAuthorized()) {
+                    if (result.isSuccessful()) {
+                        System.out.println(result.getData());
+                        messageClient = spark.messages();
+                    } else {
+                        System.out.println(result.getError());
+                        assert false;
+                    }
+                }
+                signal.countDown();
+            });
+        });
+        signal.await(30, TimeUnit.SECONDS);
     }
 
-    @Test
-    public void post() throws Exception {
-        mClient.post(null, null, "zhiyuliu@cisco.com", "Hello", null, null, result -> {
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        System.out.println("final tear down");
+        if (authenticator.isAuthorized()) {
+            authenticator.deauthorize();
+        }
+    }
+
+    private void createRoom() {
+        RoomClient roomClient = spark.rooms();
+        roomClient.create("test room", null, result -> {
             System.out.println(result);
         });
-        Thread.sleep(10 * 1000);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("tear down");
     }
 
     @Test
-    public void test() {
-
-        Gson gson = new Gson();
-        Message m = gson.fromJson("{\"roomType\":\"drr\"}", Message.class);
-
-        System.out.println(gson.toJson(m));
-
-
+    public void list() {
+        System.out.println("list");
+        if (authenticator.isAuthorized()) {
+        }
     }
 
+    @Test
+    public void post() {
+        System.out.println("post");
+        if (authenticator.isAuthorized()) {
+            messageClient.post("xionxiao@cisco.com", "hello world!", null, null, result -> {
+                System.out.println(result);
+            });
+        }
+    }
+
+    @Test
+    public void downloadFile() {
+    }
+
+    @Test
+    public void downloadThumbnail() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void delete() {
+    }
 }
